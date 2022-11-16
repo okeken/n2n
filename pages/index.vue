3a986b30f0a5793500488e73779b728200c28a07
oke kehinde <okekehinde@gmail.com>
2022-11-16 22:29:53 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <line-chartjs :chart-data="chartData" :chart-options="chartOptions" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  setup() {
+    const chartData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "Birt Rate",
+          backgroundColor: "#f87979",
+          data: [40, 39, 10, 40, 39, 80, 40],
+        },
+      ],
+    };
+
+    const chartOptions = {
+      responsive: true,
+      maintainAspectRatio: false,
+    };
+    return { chartData, chartOptions };
+  },
+};
+</script>
